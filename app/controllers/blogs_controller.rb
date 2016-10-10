c434da0d7e72fbb6ba544be161d4bf9b54b4826d
ScortiHuang <item0932@gmail.com>
2016-10-10 19:36:41 +0800

    打instagram api撈到instagram資料

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,8 @@
 class BlogsController < ApplicationController
   def index
-
+    @instagram_posts = Instagram::InstagramPosts.new('self','635574010.1677ed0.cdd432f04d92473cac4e6927dd47bfde
+').get_posts
+    @posts = JSON.parse(@instagram_posts)
   end
 
   def about
