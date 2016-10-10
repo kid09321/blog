fce6b043b103a9c05048299d6b406343dfa1aa46
ScortiHuang <item0932@gmail.com>
2016-10-10 21:01:26 +0800

    前台秀文章

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,6 +3,8 @@ class BlogsController < ApplicationController
     @instagram_posts = Instagram::InstagramPosts.new('self','635574010.1677ed0.cdd432f04d92473cac4e6927dd47bfde
 ').get_posts
     @posts = JSON.parse(@instagram_posts)['data']
+
+    @articles = Article.all
   end
 
   def about
