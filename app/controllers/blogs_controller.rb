fe731b710a4f44a48cd73f041b5dbb421961ae04
ScortiHuang <item0932@gmail.com>
2016-10-10 20:27:12 +0800

    把撈到的instagram資料render到首頁

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,7 +2,7 @@ class BlogsController < ApplicationController
   def index
     @instagram_posts = Instagram::InstagramPosts.new('self','635574010.1677ed0.cdd432f04d92473cac4e6927dd47bfde
 ').get_posts
-    @posts = JSON.parse(@instagram_posts)
+    @posts = JSON.parse(@instagram_posts)['data']
   end
 
   def about
