aa81c9b42bb78e852c3cead6b57a20e9b6e4e461
ScortiHuang <item0932@gmail.com>
2016-10-27 21:48:28 +0800

    新增分頁功能，copyright，和instagram的頁面調整，以及點擊文章標題就可瀏覽文章內容

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,11 +3,16 @@ class BlogsController < ApplicationController
   before_action :all_categories
 
   def index
+    @page = params[:page].present? ? params[:page] : 1
+    @page = @page.to_i
+    Rails.logger.info("========page#{@page}")
+    @articles_per_page = 4
+    @all_pages = (Article.all.size.to_f / @articles_per_page).ceil
+    Rails.logger.info("========all-page#{@all_pages}")
     @instagram_posts = Instagram::InstagramPosts.new('self','635574010.1677ed0.cdd432f04d92473cac4e6927dd47bfde
 ').get_posts
     @posts = JSON.parse(@instagram_posts)['data']
-
-    @articles = Article.all
+    @articles = Article.all.limit(@articles_per_page).offset(@articles_per_page * @page - 4)
     @slider_link_1 = Link.where(link_type: 'slider-1').first
     @slider_link_2 = Link.where(link_type: 'slider-2').first
     @slider_link_3 = Link.where(link_type: 'slider-3').first
